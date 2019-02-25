71952ba00a1327022370018386d8160fc4285b7f
hw <hw@questmobile.com.cn>
2019-02-25 14:33:56 +0800

    基于Redission的分布式锁

--- spring-boot-redisson/src/main/java/com/hw/springbootredisson/RedissonController.java
+++ spring-boot-redisson/src/main/java/com/hw/springbootredisson/RedissonController.java
@@ -1,5 +1,6 @@
 package com.hw.springbootredisson;
 
+import org.redisson.api.RLock;
 import org.redisson.api.RMap;
 import org.redisson.api.RedissonClient;
 import org.redisson.client.codec.StringCodec;
@@ -7,9 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.BoundHashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Description TODO
@@ -33,12 +39,49 @@ public class RedissonController {
         redissonClient.getKeys().flushall();
         RMap<String, String> map = redissonClient.getMap("test", StringCodec.INSTANCE);
         map.put("1", "2");
+        map.put("2", "3");
+        map.put("3", "4");
 
         BoundHashOperations<String, String, String> hashOperations = redisTemplate.boundHashOps("test");
-        String t = hashOperations.get("1");
-        Assert.isTrue(t.equals("2"),"是否相等");
+        String t1 = hashOperations.get("1");
+        String t2 = hashOperations.get("2");
+        String t3 = hashOperations.get("3");
 
-        return ResponseEntity.ok(t);
+        return ResponseEntity.ok(t1 + '_' + t2 + '_' + t3);
+    }
+
+    @RequestMapping("distributedLock/{count}")
+    public ResponseEntity distributedLock(@PathVariable int count) {
+        long startTime = System.currentTimeMillis();
+        CountDownLatch countDownLatch = new CountDownLatch(count);
+        ThreadPoolExecutor service = new ThreadPoolExecutor(
+                count, count, 0, TimeUnit.SECONDS, new LinkedBlockingQueue<>(),
+                Thread::new, new ThreadPoolExecutor.DiscardOldestPolicy()
+        );
+        RLock lock = redissonClient.getLock("anyLock");
+        boolean ifLock = count % 2 == 0;
+        for (int i = 0; i < count; i++) {
+            service.execute(() -> {
+                try {
+                    countDownLatch.await();
+                    if (ifLock) {
+                        lock.lock();
+                    }
+                    System.out.println(Thread.currentThread().getName() + " get distributed lock... " + System.currentTimeMillis());
+                    Thread.sleep(5);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    System.out.println(Thread.currentThread().getName() + " del distributed lock... " + System.currentTimeMillis());
+                    if (ifLock) {
+                        lock.unlock();
+                    }
+                }
+            });
+            countDownLatch.countDown();
+        }
+        service.shutdown();
+        return ResponseEntity.ok(count);
     }
 
 }
--- spring-boot-redisson/src/test/java/com/hw/springbootredisson/RedissonAutoConfigurationTest.java
+++ spring-boot-redisson/src/test/java/com/hw/springbootredisson/RedissonAutoConfigurationTest.java
@@ -1,11 +1,50 @@
 package com.hw.springbootredisson;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.redisson.api.RMap;
+import org.redisson.api.RedissonClient;
+import org.redisson.client.codec.StringCodec;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.BoundHashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
  * @Description TODO
  * @Author hw
  * @Date 2019/2/22 18:28
  * @Version 1.0
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest(
+        classes = SpringBootRedissonApplication.class,
+        properties = {
+                "spring.redis.redisson.config=classpath:redisson.yaml",
+                "spring.redis.timeout=10000"
+        })
 public class RedissonAutoConfigurationTest {
+
+    @Autowired
+    private RedissonClient redisson;
+
+    @Autowired
+    private RedisTemplate<String, String> template;
+
+    @Test
+    public void testApp() {
+        redisson.getKeys().flushall();
+
+        RMap<String, String> m = redisson.getMap("test", StringCodec.INSTANCE);
+        m.put("1", "2");
+
+        BoundHashOperations<String, String, String> hash = template.boundHashOps("test");
+        String t = hash.get("1");
+        System.out.println(t);
+        assertThat(t).isEqualTo("2");
+    }
 
 }
